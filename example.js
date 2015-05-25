e676541f6d9d636137bfa9dc25121d08861b2c7c
Amely Kling <mail@dwi.sk>
2015-05-25 23:19:06 +0200

    Example script

---
+++ example.js
@@ -0,0 +1,35 @@
+/*
+ * Example showing a simple loop reading the touch status every 0.1ms
+ */
+
+
+// get library
+var MPR121 = require('./index.js');
+
+// setup sensor device id
+var touchsensor = new MPR121(0x52);
+
+// initiialze sensor, on success start script
+if (touchsensor.begin()) {
+	// message how to quit
+	console.log('Press Ctrl-C to quit.');
+
+	// Interval for reading the sonsor
+	setInterval(function() {
+		// get touch values
+		var t = touchsensor.touched();
+
+		// prepare some result array
+		var ret = [];
+
+		// loop through pins
+		for (var i = 0; i < 12; i++) {
+			// push status into sensor
+			ret.push (touchsensor.is_touched(i));
+		}
+		
+		// return status
+		console.log(ret);
+
+	},100);
+};
